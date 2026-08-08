7ab1c43e55e2cbd45598389bd8db34514dd80abc
William Neddo <chevylover1001@gmail.com>
2026-08-08 19:20:30 +0200

    fix: sort lang keys and refine error boundary

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_quiz_assistant';
-$plugin->version = 2026080801;
+$plugin->version = 2026080802;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.1';
+$plugin->release = '0.1.2';
