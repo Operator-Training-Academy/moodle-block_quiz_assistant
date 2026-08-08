19896aef52caa1ff121e29d9bd0382e41c33423e
William Neddo <chevylover1001@gmail.com>
2026-08-08 19:22:43 +0200

    fix: alphabetical order of language strings

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_quiz_assistant';
-$plugin->version = 2026080802;
+$plugin->version = 2026080803;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.2';
+$plugin->release = '0.1.3';
